af87b007e46f3b9e3e5ce7e05807051ddcf8b0c9
JayjeetAtGithub <jc.github@rediffmail.com>
2024-10-08 11:52:56 -0700

    Add faiss install script

---
+++ install_faiss.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ex
+
+faiss_dir=$1
+
+cd ${faiss_dir}
+
+rm -rf build/
+mkdir -p build/
+cd build/
+
+sudo apt-get install -y libblas-dev libopenblas-dev liblapack-dev swig python3-numpy
+
+cmake -DFAISS_ENABLE_GPU=OFF \
+      -DFAISS_ENABLE_PYTHON=OFF \
+      -DBUILD_SHARED_LIBS=ON \
+      -DFAISS_OPT_LEVEL=avx2 \
+      -DBUILD_TESTING=OFF \
+      -DCMAKE_BUILD_TYPE=Release \
+     ..
+
+sudo make -j$(nproc)
+sudo make install
