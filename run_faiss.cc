5143a953d7f6147beed61ddcb937d85e1c27e301
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-10-09 16:25:24 +0000

    Add CLI lib

--- run_faiss.cc
+++ run_faiss.cc
@@ -14,43 +14,71 @@
 #include <faiss/gpu/GpuIndexIVFFlat.h>
 #include <faiss/gpu/StandardGpuResources.h>
 #include <faiss/index_io.h>
+#include "CLI11.hpp"
 
 #include "utils.h"
 
 faiss::Index *CPU_create_ivf_flat_index(size_t dim, size_t nlist,
                                         size_t nprobe) {
-  return new faiss::IndexIVFFlat(new faiss::IndexFlatL2(dim), dim, nlist);
+  auto quantizer = new faiss::IndexFlatL2(dim);
+  return new faiss::IndexIVFFlat(quantizer, dim, nlist, faiss::METRIC_L2);
+}
+
+faiss::Index *GPU_create_ivf_flat_index(size_t dim, size_t nlist, size_t nprobe, std::string mem_type, int32_t cuda_device) {
+  auto res = new faiss::gpu::StandardGpuResources();
+  auto quantizer = new faiss::gpu::GpuIndexFlatL2(res, dim);
+  auto config = faiss::gpu::GpuIndexIVFFlatConfig();
+  config.device = cuda_device;
+  config.memorySpace = (mem_type == "cuda") ? faiss::gpu::MemorySpace::Device : faiss::gpu::MemorySpace::Unified;
+  return new faiss::gpu::GpuIndexIVFFlat(
+    res, quantizer, dim, nlist, faiss::METRIC_L2, config);
 }
 
 int main(int argc, char **argv) {
-  if (argc != 5) {
-    std::cerr << "Usage: " << argv[0] << " <dataset> <mode> <limit> <top_k>"
-              << std::endl;
-    std::exit(1);
+  CLI::App app{"Run FAISS Benchmarks"};
+  argv = app.ensure_utf8(argv);
+
+  std::string op;
+  app.add_option("-o,--op", op, "Operation to perform: index or search");
+
+  std::string dataset;
+  app.add_option("-d,--dataset", dataset, "Path to the dataset");
+
+  std::string mode = "cpu";
+  app.add_option("-m,--mode", mode, "Mode: cpu or gpu");
+
+  std::string mem_type = "cuda";
+  app.add_option("-t,--mem_type", mem_type, "Memory type: cuda or managed");
+
+  int32_t cuda_device = 0;
+  app.add_option("-c,--cuda_device", cuda_device, "The CUDA device to use");
+
+  uint32_t limit = 1000;
+  app.add_option("-l,--limit", limit, "Limit the number of vectors");
+
+  int32_t top_k = 10;
+  app.add_option("-k,--top_k", top_k, "Number of nearest neighbors");
+
+  CLI11_PARSE(app, argc, argv);
+
+  if (op.empty()) {
+    std::cerr << "[ERROR] Please provide an operation" << std::endl;
+    return 1;
+  }
+  if (dataset.empty()) {
+    std::cerr << "[ERROR] Please provide a dataset" << std::endl;
+    return 1;
   }
 
-  std::string dataset = argv[1];
-  std::string mode = argv[2];
-  uint32_t limit = std::atoi(argv[3]);
-  int32_t top_k = std::atoi(argv[4]);
-
-  // Print the arguments
-  std::cout << "[ARG] dataset: " << dataset << std::endl;
-  std::cout << "[ARG] mode: " << mode << std::endl;
-  std::cout << "[ARG] limit: " << limit << std::endl;
-  std::cout << "[ARG] top_k: " << top_k << std::endl;
-
-  std::cout << std::endl;
-
-  // Preparing GPU resources
-  auto const n_gpus = faiss::gpu::getNumDevices();
-  std::cout << "[INFO] Number of GPUs: " << n_gpus << std::endl;
-  std::vector<faiss::gpu::GpuResourcesProvider *> res;
-  std::vector<int> devs;
-  for (int i = 0; i < n_gpus; i++) {
-    res.push_back(new faiss::gpu::StandardGpuResources());
-    devs.push_back(i);
-  }
+  // // Preparing GPU resources
+  // auto const n_gpus = faiss::gpu::getNumDevices();
+  // std::cout << "[INFO] Number of GPUs: " << n_gpus << std::endl;
+  // std::vector<faiss::gpu::GpuResourcesProvider *> res;
+  // std::vector<int> devs;
+  // for (int i = 0; i < n_gpus; i++) {
+  //   res.push_back(new faiss::gpu::StandardGpuResources());
+  //   devs.push_back(i);
+  // }
 
   // Load the learn dataset
   uint32_t dim_learn, n_learn;
@@ -65,16 +93,11 @@ int main(int argc, char **argv) {
   preview_dataset(data_learn);
 
   // Create the index
-  auto idx = CPU_create_ivf_flat_index(dim_learn, 1024, 100);
-  if (mode == "gpu_multiple") {
-    idx = faiss::gpu::index_cpu_to_gpu_multiple(res, devs, idx);
-  } else if (mode == "gpu_single") {
-    idx = faiss::gpu::index_cpu_to_gpu(res[0], devs[0], idx);
-  } else if (mode == "cpu") {
-    // Do nothing
+  faiss::Index *idx;
+  if (mode == "cpu") {
+    idx = CPU_create_ivf_flat_index(dim_learn, 1024, 100);
   } else {
-    std::cerr << "[ERROR] Invalid mode" << std::endl;
-    std::exit(1);
+    idx = GPU_create_ivf_flat_index(dim_learn, 1024, 100, mem_type, cuda_device);
   }
 
   // Train the index
@@ -97,6 +120,11 @@ int main(int argc, char **argv) {
 
   // Delete the learn dataset
   delete[] data_learn;
+
+  // Save the index
+  std::string index_path = "index.faiss";
+  faiss::gpu::index_gpu_to_cpu(idx);
+  faiss::write_index(idx, index_path.c_str());
 
   std::cout << std::endl;
 
