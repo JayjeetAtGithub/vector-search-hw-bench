0d55e24ff107690df83d015df6a332c0630e97b8
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-01-23 15:38:59 +0530

    make learn limit and search limit configurabke

--- cpu_hnsw/run_faiss_cpu.sh
+++ cpu_hnsw/run_faiss_cpu.sh
@@ -3,11 +3,13 @@ set -e
 
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
+learn_limit=1000000
+search_limit=10000
 
 ./run_faiss_cpu \
     --dataset-dir /workspace/dataset/gist \
-    --learn-limit 1000000 \
-    --search-limit 10000 \
+    --learn-limit ${learn_limit} \
+    --search-limit ${search_limit} \
     --top-k 10 \
     --ef 256 \
     --metric 1 \
@@ -18,8 +20,8 @@ perf stat -e fp_arith_inst_retired.512b_packed_single \
           -e fp_arith_inst_retired.scalar_single \
           ./run_faiss_cpu \
             --dataset-dir /workspace/dataset/gist \
-            --learn-limit 1000000 \
-            --search-limit 10000 \
+            --learn-limit ${learn_limit} \
+            --search-limit ${search_limit} \
             --top-k 10 \
             --ef 256 \
             --metric 1 \
