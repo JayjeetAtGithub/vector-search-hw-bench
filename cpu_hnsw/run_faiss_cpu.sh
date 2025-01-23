6b8c52e0ea77faad51fea21e217da9b48f6ef4b3
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-01-23 15:37:50 +0530

    Fixes

--- cpu_hnsw/run_faiss_cpu.sh
+++ cpu_hnsw/run_faiss_cpu.sh
@@ -3,6 +3,16 @@ set -e
 
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
+
+./run_faiss_cpu \
+    --dataset-dir /workspace/dataset/gist \
+    --learn-limit 1000000 \
+    --search-limit 10000 \
+    --top-k 10 \
+    --ef 256 \
+    --metric 1 \
+    --index-file hnsw_idx.faiss
+
 perf stat -e fp_arith_inst_retired.512b_packed_single \
           -e fp_arith_inst_retired.vector \
           -e fp_arith_inst_retired.scalar_single \
@@ -12,4 +22,6 @@ perf stat -e fp_arith_inst_retired.512b_packed_single \
             --search-limit 10000 \
             --top-k 10 \
             --ef 256 \
-            --metric 1
+            --metric 1 \
+            --skip-build \
+            --index-file hnsw_idx.faiss
