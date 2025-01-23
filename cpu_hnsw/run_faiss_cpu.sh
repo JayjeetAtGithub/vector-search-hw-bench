1afd4ef0441c003fe71961e9b9839fa6bfa44a33
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-01-23 12:34:06 +0530

    Fixes

--- cpu_hnsw/run_faiss_cpu.sh
+++ cpu_hnsw/run_faiss_cpu.sh
@@ -5,7 +5,6 @@ export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 ./run_faiss_cpu \
     --dataset-dir /workspace/dataset/gist \
-    --query-dir /workspace/dataset/gist \
     --learn-limit 1000000 \
     --search-limit 10000 \
     --top-k 10 \
