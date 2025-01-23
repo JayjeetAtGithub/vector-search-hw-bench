3ebd1c76332659d76ed2b761399d5589da9fde07
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-01-23 13:25:00 +0530

    Use avx512

--- cpu_hnsw/run_faiss_cpu.sh
+++ cpu_hnsw/run_faiss_cpu.sh
@@ -3,9 +3,12 @@ set -e
 
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-perf stat -e fp_arith_inst_retired.512b_packed_single ./run_faiss_cpu \
-    --dataset-dir /workspace/dataset/gist \
-    --learn-limit 1000000 \
-    --search-limit 10000 \
-    --top-k 10 \
-    --ef 256
+perf stat -e fp_arith_inst_retired.512b_packed_single \
+          -e fp_arith_inst_retired.vector \
+          -e fp_arith_inst_retired.scalar_single \
+          ./run_faiss_cpu \
+            --dataset-dir /workspace/dataset/gist \
+            --learn-limit 1000000 \
+            --search-limit 10000 \
+            --top-k 10 \
+            --ef 256
