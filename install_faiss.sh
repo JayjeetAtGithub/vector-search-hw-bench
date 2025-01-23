76c27bb745d90df25dbd2777a307a7a1b78048ce
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-01-23 13:25:29 +0530

    Use avx512

--- install_faiss.sh
+++ install_faiss.sh
@@ -15,7 +15,7 @@ sudo apt-get install -y libssl-dev libblas-dev libopenblas-dev liblapack-dev swi
 cmake -DFAISS_ENABLE_GPU=OFF \
       -DFAISS_ENABLE_PYTHON=OFF \
       -DBUILD_SHARED_LIBS=ON \
-      -DFAISS_OPT_LEVEL=avx512 \
+      -DFAISS_OPT_LEVEL=avx512_spr \
       -DBUILD_TESTING=OFF \
       -DCMAKE_BUILD_TYPE=Release \
      ..
