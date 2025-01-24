0c6a83ab9170144f308bf527266ec0d796e6a0db
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-01-24 15:30:29 +0530

    Fixes

--- cpu_hnsw/run_faiss_cpu.sh
+++ cpu_hnsw/run_faiss_cpu.sh
@@ -3,16 +3,17 @@ set -e
 
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-learn_limit=1000000
-search_limit=10000
+learn_limit=100000
+search_limit=1000
 
 ./run_faiss_cpu \
+    --index-type hnsw \
     --dataset-dir /workspace/dataset/gist \
     --learn-limit ${learn_limit} \
     --search-limit ${search_limit} \
     --top-k 10 \
     --ef 256 \
-    --metric 1 \
+    --metric ip \
     --index-file cpu_hnsw.faiss
 
 perf stat -e fp_arith_inst_retired.512b_packed_single \
@@ -28,3 +29,51 @@ perf stat -e fp_arith_inst_retired.512b_packed_single \
             --metric ip \
             --skip-build 1 \
             --index-file cpu_hnsw.faiss
+
+./run_faiss_cpu \
+    --index-type ivf \
+    --dataset-dir /workspace/dataset/gist \
+    --learn-limit ${learn_limit} \
+    --search-limit ${search_limit} \
+    --top-k 10 \
+    --ef 256 \
+    --metric ip \
+    --index-file cpu_ivf.faiss
+
+perf stat -e fp_arith_inst_retired.512b_packed_single \
+          -e fp_arith_inst_retired.vector \
+          -e fp_arith_inst_retired.scalar_single \
+          ./run_faiss_cpu \
+            --index-type ivf \
+            --dataset-dir /workspace/dataset/gist \
+            --learn-limit ${learn_limit} \
+            --search-limit ${search_limit} \
+            --top-k 10 \
+            --ef 256 \
+            --metric ip \
+            --skip-build 1 \
+            --index-file cpu_ivf.faiss
+
+./run_faiss_cpu \
+    --index-type flat \
+    --dataset-dir /workspace/dataset/gist \
+    --learn-limit ${learn_limit} \
+    --search-limit ${search_limit} \
+    --top-k 10 \
+    --ef 256 \
+    --metric ip \
+    --index-file cpu_flat.faiss
+
+perf stat -e fp_arith_inst_retired.512b_packed_single \
+          -e fp_arith_inst_retired.vector \
+          -e fp_arith_inst_retired.scalar_single \
+          ./run_faiss_cpu \
+            --index-type flat \
+            --dataset-dir /workspace/dataset/gist \
+            --learn-limit ${learn_limit} \
+            --search-limit ${search_limit} \
+            --top-k 10 \
+            --ef 256 \
+            --metric ip \
+            --skip-build 1 \
+            --index-file cpu_flat.faiss
