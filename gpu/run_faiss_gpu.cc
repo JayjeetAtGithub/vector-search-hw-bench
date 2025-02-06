c79c1adb40d176cb8e5803193e8d6ad6b15c8554
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-02-06 11:59:41 +0530

    Fixes

--- gpu/run_faiss_gpu.cc
+++ gpu/run_faiss_gpu.cc
@@ -4,52 +4,56 @@
 #include <vector>
 
 #include "CLI11.hpp"
-#include <faiss/IndexFlat.h>
-#include <faiss/IndexHNSW.h>
-#include <faiss/IndexIVFFlat.h>
+#include <faiss/gpu/GpuCloner.h>
+#include <faiss/gpu/GpuIndexFlat.h>
+#include <faiss/gpu/GpuIndexIVFFlat.h>
+#include <faiss/gpu/StandardGpuResources.h>
 #include <faiss/index_io.h>
 
 #include "utils.h"
 
 /**
- * @brief Create a HNSW index using the CPU
+ * @brief Create a Flat index using the GPU
  *
  * @param dim The dimension of the vectors
- * @param ef The number of neighbors to explore
- * @param dis_metric The distance metric to use
+ * @param mem_type The memory type to use
+ * @param provider The GPU resources provider
+ * @param cuda_device The CUDA device to use
  */
-faiss::Index *CPU_create_hnsw_index(int64_t dim, std::string dis_metric) {
-  // Use the default value of M in FAISS
-  auto faiss_metric_type = (dis_metric == "l2") ? faiss::MetricType::METRIC_L2
-                                             : faiss::MetricType::METRIC_INNER_PRODUCT;
-  auto index = new faiss::IndexHNSWFlat(dim, 32, faiss_metric_type);
-  // Use the default value of efConstruction in FAISS
-  index->hnsw.efConstruction = 40;
+faiss::Index *GPU_create_flat_index(int64_t dim, std::string mem_type,
+                                    faiss::gpu::GpuResourcesProvider *provider,
+                                    int64_t cuda_device) {
+  auto config = faiss::gpu::GpuIndexConfig();
+  config.device = cuda_device;
+  config.memorySpace = (mem_type == "cuda") ? faiss::gpu::MemorySpace::Device
+                                            : faiss::gpu::MemorySpace::Unified;
+  auto index = new faiss::gpu::GpuIndexFlatL2(
+      provider, dim, faiss::gpu::GpuIndexFlatConfig{config});
   return index;
 }
 
 /**
-  * @brief Create an IVF index using the CPU
-  *
-  * @param dim The dimension of the vectors
-  * @param nlist The number of cells in the inverted file
-  * @param dis_metric The distance metric to use
-  */
-faiss::Index *CPU_create_ivf_index(int64_t dim, int64_t nlist, std::string dis_metric) {
-  auto faiss_metric_type = (dis_metric == "l2") ? faiss::METRIC_L2 : faiss::METRIC_INNER_PRODUCT;
-  auto index = new faiss::IndexIVFFlat(new faiss::IndexFlatL2(dim), dim, nlist, faiss_metric_type);
+ * @brief Create an IVF index using the GPU
+ *
+ * @param dim The dimension of the vectors
+ * @param nlist The number of inverted lists
+ * @param mem_type The memory type to use
+ * @param provider The GPU resources provider
+ * @param cuda_device The CUDA device to use
+ */
+faiss::Index *GPU_create_ivf_index(
+    int64_t dim, int64_t nlist, std::string mem_type,
+    faiss::gpu::GpuResourcesProvider *provider, int64_t cuda_device) {
+  auto config = faiss::gpu::GpuIndexConfig();
+  config.device = cuda_device;
+  config.memorySpace = (mem_type == "cuda") ? faiss::gpu::MemorySpace::Device
+                                            : faiss::gpu::MemorySpace::Unified;
+  auto quantizer = new faiss::gpu::GpuIndexFlatL2(
+      provider, dim, faiss::gpu::GpuIndexFlatConfig{config});
+  auto index = new faiss::gpu::GpuIndexIVFFlat(
+      provider, quantizer, dim, nlist, faiss::METRIC_L2,
+      faiss::gpu::GpuIndexIVFFlatConfig{config});
   return index;
-}
-
-/**
-  * @brief Create a flat index using the CPU
-  *
-  * @param dim The dimension of the vectors
-  * @param dis_metric The distance metric to use
-  */
-faiss::Index *CPU_create_flat_index(int64_t dim, std::string dis_metric) {
-  auto faiss_metric_type = (dis_metric == "l2") ? faiss::METRIC_L2 : faiss::METRIC_INNER_PRODUCT;
-  return new faiss::IndexFlat(dim, faiss_metric_type);
 }
 
 int main(int argc, char **argv) {
@@ -98,6 +102,9 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  // Preparing GPU resources
+  auto provider = new faiss::gpu::StandardGpuResources();
+
   if (!skip_build) {
     // Load the learn dataset
     std::string dataset_path_learn = dataset_dir + "/dataset.bin";
@@ -114,13 +121,11 @@ int main(int argc, char **argv) {
 
     // Create the index
     faiss::Index *widx;
-    if (index_type == "hnsw") {
-      widx = CPU_create_hnsw_index(dim_learn, dis_metric);
-    } else if (index_type == "ivf") {
-      widx = CPU_create_ivf_index(dim_learn, n_list, dis_metric);
+    if (index_type == "ivf") {
+      widx = GPU_create_ivf_index(dim_learn, n_list, dis_metric);
       widx->train(n_learn, data_learn.data());
     } else if (index_type == "flat") {
-      widx = CPU_create_flat_index(dim_learn, dis_metric);
+      widx = GPU_create_flat_index(dim_learn, dis_metric);
     } else {
       std::cerr << "[ERROR] Invalid index type" << std::endl;
       return 1;
@@ -136,67 +141,29 @@ int main(int argc, char **argv) {
         << " ms" << std::endl;
 
     // Save the index to disk
-    faiss::write_index(widx, index_file.c_str());
+    cpu_widx = faiss::gpu::index_gpu_to_cpu(widx);
+    faiss::write_index(cpu_widx, index_file.c_str());
   }
-
-  if (skip_build) {
-    // Read the index from disk
-    faiss::Index *ridx = faiss::read_index(index_file.c_str());
-    if (index_type == "ivf") {
-      dynamic_cast<faiss::IndexIVFFlat*>(ridx)->nprobe = n_probe;
-    } else if (index_type == "hnsw") {
-      dynamic_cast<faiss::IndexHNSWFlat*>(ridx)->hnsw.efSearch = ef;
-    }
-
-    // Load the search dataset
-    std::string dataset_path_query = dataset_dir + "/query.bin";
-    int64_t n_query, dim_query;
-    auto data_query = read_bin_dataset(dataset_path_query.c_str(), &n_query, &dim_query, search_limit);
-
-    // Print information about the search dataset
-    std::cout << "[INFO] Query dataset shape: " << dim_query << " x " << n_query
-              << std::endl;
-    preview_dataset(data_query);
-
-    // Containers to hold the search results
-    std::vector<faiss::idx_t> nns(top_k * n_query);
-    std::vector<float> dis(top_k * n_query);
-
-    // Perform the search
-    auto s = std::chrono::high_resolution_clock::now();
-    for (int itr = 0; itr < 100; itr++) {
-      ridx->search(n_query, data_query.data(), top_k, dis.data(), nns.data());
-    }
-    auto e = std::chrono::high_resolution_clock::now();
-    std::cout
-        << "[TIME] Search: "
-        << std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count()
-        << " ms" << std::endl;
-
-    if (calc_recall == "true") {
-      std::string dataset_path_learn = dataset_dir + "/dataset.bin";
-      int64_t n_learn, dim_learn;
-      auto data_learn = read_bin_dataset(dataset_path_learn.c_str(), &n_learn, &dim_learn, learn_limit);
-
-      faiss::Index *gt_idx = CPU_create_flat_index(dim_learn, dis_metric);
-      gt_idx->add(n_learn, data_learn.data());
-      std::vector<faiss::idx_t> gt_nns(top_k * n_query);
-      std::vector<float> gt_dis(top_k * n_query);
-      gt_idx->search(n_query, data_query.data(), top_k, gt_dis.data(), gt_nns.data());
-      int64_t recalls = 0;
-      for (int64_t i = 0; i < n_query; ++i) {
-        for (int64_t n = 0; n < top_k; n++) {
-          for (int64_t m = 0; m < top_k; m++) {
-            if (nns[i * top_k + n] == gt_nns[i * top_k + m]) {
-              recalls += 1;
-            }
-          }
-        }
-      }
-      float recall = 1.0f * recalls / (top_k * n_query);
-      std::cout << "[INFO] Recall@" << top_k << ": " << recall << std::endl;
-    }
-  }
-
-  return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
