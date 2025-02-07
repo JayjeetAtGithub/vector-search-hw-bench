5e8a02f3f78cd8d47a07863ef251300716cafa60
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-02-07 10:17:32 +0530

    Fixes

--- gpu/run_faiss_gpu.sh
+++ gpu/run_faiss_gpu.sh
@@ -3,48 +3,60 @@ set -e
 
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-learn_limit=100000
-search_limit=1000
-calc_recall=${1:-false}
-
-./run_faiss_gpu \
-    --index-type ivf \
-    --dataset-dir /workspace/dataset/deep1b \
-    --learn-limit ${learn_limit} \
-    --search-limit ${search_limit} \
-    --top-k 10 \
-    --n-probe 128 \
-    --metric ip \
-    --index-file gpu_ivf.faiss
-
-./run_faiss_gpu \
-        --index-type ivf \
-        --dataset-dir /workspace/dataset/deep1b \
-        --learn-limit ${learn_limit} \
-        --search-limit ${search_limit} \
-        --top-k 10 \
-        --n-probe 128 \
-        --metric ip \
-        --skip-build 1 \
-        --index-file gpu_ivf.faiss \
-        --calc-recall ${calc_recall}
-
-./run_faiss_gpu \
-    --index-type flat \
-    --dataset-dir /workspace/dataset/deep1b \
-    --learn-limit ${learn_limit} \
-    --search-limit ${search_limit} \
-    --top-k 10 \
-    --metric ip \
-    --index-file gpu_flat.faiss
-
-./run_faiss_gpu \
+run_flat() {
+    ./run_faiss_gpu \
         --index-type flat \
         --dataset-dir /workspace/dataset/deep1b \
-        --learn-limit ${learn_limit} \
-        --search-limit ${search_limit} \
+        --learn-limit ${1} \
+        --search-limit ${2} \
+        --top-k 10 \
+        --metric ip \
+        --index-file gpu_flat_${1}l_${2}q.faiss
+
+    ./run_faiss_gpu \
+        --index-type flat \
+        --dataset-dir /workspace/dataset/deep1b \
+        --learn-limit ${1} \
+        --search-limit ${2} \
         --top-k 10 \
         --metric ip \
         --skip-build 1 \
-        --index-file gpu_flat.faiss \
-        --calc-recall ${calc_recall}
+        --index-file gpu_flat_${1}l_${2}q.faiss \
+        --calc-recall true
+}
+
+run_ivf() {
+    ./run_faiss_gpu \
+        --index-type ivf \
+        --dataset-dir /workspace/dataset/deep1b \
+        --learn-limit ${1} \
+        --search-limit ${2} \
+        --top-k 10 \
+        --n-probe ${3} \
+        --metric ip \
+        --index-file gpu_ivf_${1}l_${2}q.faiss
+
+    ./run_faiss_gpu \
+        --index-type ivf \
+        --dataset-dir /workspace/dataset/deep1b \
+        --learn-limit ${1} \
+        --search-limit ${2} \
+        --top-k 10 \
+        --n-probe ${3} \
+        --metric ip \
+        --skip-build 1 \
+        --index-file gpu_ivf_${1}l_${2}q.faiss \
+        --calc-recall true
+}
+
+# run flat on 100k learn vectors and 1k search vectors
+run_flat 100000 1000
+
+# run ivf on 100k learn vectors and 1k search vectors
+run_ivf 100000 1000 128
+
+# run flat on 1M learn vectors and 10k search vectors
+run_flat 1000000 10000
+
+# run ivf on 1M learn vectors and 10k search vectors
+run_ivf 1000000 10000 256
