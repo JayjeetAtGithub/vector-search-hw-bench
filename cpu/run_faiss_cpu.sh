62fa134b1dabf1878d608b0e663360961df1c43c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-02-10 13:08:08 +0530

    Make fns

--- cpu/run_faiss_cpu.sh
+++ cpu/run_faiss_cpu.sh
@@ -7,100 +7,112 @@ learn_limit=100000
 search_limit=1000
 calc_recall=${1:-false}
 
-./run_faiss_cpu \
-    --index-type hnsw \
-    --dataset-dir /workspace/dataset/deep1b \
-    --learn-limit ${learn_limit} \
-    --search-limit ${search_limit} \
-    --top-k 10 \
-    --ef 256 \
-    --metric ip \
-    --index-file cpu_hnsw.faiss
 
-perf stat -e fp_arith_inst_retired.512b_packed_single \
-          -e fp_arith_inst_retired.256b_packed_single \
-          -e fp_arith_inst_retired.128b_packed_single \
-          -e fp_arith_inst_retired.vector \
-          -e fp_arith_inst_retired.scalar_single \
-          -e cache-misses \
-          -e instructions \
-          -e cpu-cycles \
-          -e branch-instructions \
-          -e branch-misses \
-          ./run_faiss_cpu \
-            --index-type hnsw \
-            --dataset-dir /workspace/dataset/deep1b \
-            --learn-limit ${learn_limit} \
-            --search-limit ${search_limit} \
-            --top-k 10 \
-            --ef 256 \
-            --metric ip \
-            --skip-build 1 \
-            --index-file cpu_hnsw.faiss \
-            --calc-recall ${calc_recall}
-
-./run_faiss_cpu \
-    --index-type ivf \
-    --dataset-dir /workspace/dataset/deep1b \
-    --learn-limit ${learn_limit} \
-    --search-limit ${search_limit} \
-    --top-k 10 \
-    --ef 256 \
-    --n-probe 128 \
-    --metric ip \
-    --index-file cpu_ivf.faiss
-
-perf stat -e fp_arith_inst_retired.512b_packed_single \
-          -e fp_arith_inst_retired.256b_packed_single \
-          -e fp_arith_inst_retired.128b_packed_single \
-          -e fp_arith_inst_retired.vector \
-          -e fp_arith_inst_retired.scalar_single \
-          -e cache-misses \
-          -e instructions \
-          -e cpu-cycles \
-          -e branch-instructions \
-          -e branch-misses \
-          ./run_faiss_cpu \
-            --index-type ivf \
-            --dataset-dir /workspace/dataset/deep1b \
-            --learn-limit ${learn_limit} \
-            --search-limit ${search_limit} \
-            --top-k 10 \
-            --ef 256 \
-            --n-probe 128 \
-            --metric ip \
-            --skip-build 1 \
-            --index-file cpu_ivf.faiss \
-            --calc-recall ${calc_recall}
-
-./run_faiss_cpu \
+run_flat() {
+  ./run_faiss_cpu \
     --index-type flat \
     --dataset-dir /workspace/dataset/deep1b \
-    --learn-limit ${learn_limit} \
-    --search-limit ${search_limit} \
+    --learn-limit ${1} \
+    --search-limit ${2} \
     --top-k 10 \
     --ef 256 \
     --metric ip \
-    --index-file cpu_flat.faiss
+    --index-file cpu_flat_${1}l_${2}q.faiss
 
-perf stat -e fp_arith_inst_retired.512b_packed_single \
-          -e fp_arith_inst_retired.256b_packed_single \
-          -e fp_arith_inst_retired.128b_packed_single \
-          -e fp_arith_inst_retired.vector \
-          -e fp_arith_inst_retired.scalar_single \
-          -e cache-misses \
-          -e instructions \
-          -e cpu-cycles \
-          -e branch-instructions \
-          -e branch-misses \
-          ./run_faiss_cpu \
-            --index-type flat \
-            --dataset-dir /workspace/dataset/deep1b \
-            --learn-limit ${learn_limit} \
-            --search-limit ${search_limit} \
-            --top-k 10 \
-            --ef 256 \
-            --metric ip \
-            --skip-build 1 \
-            --index-file cpu_flat.faiss \
-            --calc-recall ${calc_recall}
+  perf stat -e fp_arith_inst_retired.512b_packed_single \
+            -e fp_arith_inst_retired.256b_packed_single \
+            -e fp_arith_inst_retired.128b_packed_single \
+            -e fp_arith_inst_retired.vector \
+            -e fp_arith_inst_retired.scalar_single \
+            -e cache-misses \
+            -e instructions \
+            -e cpu-cycles \
+            -e branch-instructions \
+            -e branch-misses \
+            ./run_faiss_cpu \
+              --index-type flat \
+              --dataset-dir /workspace/dataset/deep1b \
+              --learn-limit ${1} \
+              --search-limit ${2} \
+              --top-k 10 \
+              --ef 256 \
+              --metric ip \
+              --skip-build 1 \
+              --index-file cpu_flat_${1}l_${2}q.faiss \
+              --calc-recall true
+
+}
+
+run_ivf() {
+  ./run_faiss_cpu \
+      --index-type ivf \
+      --dataset-dir /workspace/dataset/deep1b \
+      --learn-limit ${1} \
+      --search-limit ${2} \
+      --top-k 10 \
+      --ef 256 \
+      --n-probe ${3} \
+      --metric ip \
+      --index-file cpu_ivf_${1}l_${2}q.faiss
+
+  perf stat -e fp_arith_inst_retired.512b_packed_single \
+            -e fp_arith_inst_retired.256b_packed_single \
+            -e fp_arith_inst_retired.128b_packed_single \
+            -e fp_arith_inst_retired.vector \
+            -e fp_arith_inst_retired.scalar_single \
+            -e cache-misses \
+            -e instructions \
+            -e cpu-cycles \
+            -e branch-instructions \
+            -e branch-misses \
+            ./run_faiss_cpu \
+              --index-type ivf \
+              --dataset-dir /workspace/dataset/deep1b \
+              --learn-limit ${1} \
+              --search-limit ${2} \
+              --top-k 10 \
+              --ef 256 \
+              --n-probe ${3} \
+              --metric ip \
+              --skip-build 1 \
+              --index-file cpu_ivf_${1}l_${2}q.faiss \
+              --calc-recall true
+}
+
+run_hnsw() {
+  ./run_faiss_cpu \
+      --index-type hnsw \
+      --dataset-dir /workspace/dataset/deep1b \
+      --learn-limit ${learn_limit} \
+      --search-limit ${search_limit} \
+      --top-k 10 \
+      --ef 256 \
+      --metric ip \
+      --index-file cpu_hnsw_${1}l_${2}q.faiss
+
+  perf stat -e fp_arith_inst_retired.512b_packed_single \
+            -e fp_arith_inst_retired.256b_packed_single \
+            -e fp_arith_inst_retired.128b_packed_single \
+            -e fp_arith_inst_retired.vector \
+            -e fp_arith_inst_retired.scalar_single \
+            -e cache-misses \
+            -e instructions \
+            -e cpu-cycles \
+            -e branch-instructions \
+            -e branch-misses \
+            ./run_faiss_cpu \
+              --index-type hnsw \
+              --dataset-dir /workspace/dataset/deep1b \
+              --learn-limit ${1} \
+              --search-limit ${2} \
+              --top-k 10 \
+              --ef 256 \
+              --metric ip \
+              --skip-build 1 \
+              --index-file cpu_hnsw_${1}l_${2}q.faiss \
+              --calc-recall true
+}
+
+run_flat 1000000 10000 
+run_ivf 1000000 10000 256
+run_hnsw 1000000 10000
