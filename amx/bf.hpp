ac1d93313e186741465447e4641d6020c688e807
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-02-19 11:30:15 +0530

    Add amx code

---
+++ amx/bf.hpp
@@ -0,0 +1,73 @@
+#include <queue>
+#include <vector>
+
+#include "distance.hpp"
+
+class BruteForceSearch {
+  int32_t _dim;
+  std::vector<std::vector<float>> _dataset;
+
+  dnnl::engine engine;
+  dnnl::stream stream;
+
+public:
+  void init_onednn() {
+    engine = dnnl::engine(dnnl::engine::kind::cpu, 0);
+    stream = dnnl::stream(engine);
+  }
+
+  BruteForceSearch(int32_t dim) : _dim(dim) {
+    init_onednn();
+    if (!is_amxbf16_supported()) {
+      std::cout << "Intel AMX unavailable" << std::endl;
+    }
+  }
+
+  void add(std::vector<float> point) { _dataset.push_back(point); }
+
+  std::pair<int32_t, int32_t> shape() {
+    return std::make_pair(_dataset.size(), _dataset[0].size());
+  }
+
+  std::vector<std::vector<float>> search_ip_amx(std::vector<std::vector<float>> queries, int32_t top_k) {
+    std::vector<std::vector<float>> results(queries.size(),
+                                            std::vector<float>(top_k, 0.0f));
+    std::unordered_map<int32_t, std::priority_queue<float, std::vector<float>,
+                                                    std::greater<float>>>
+        map;
+
+    std::vector<bf16> mat_a(queries.size() * _dim);
+    std::vector<bf16> mat_b(_dataset.size() * _dim);
+
+    for (int32_t i = 0; i < queries.size(); i++) {
+      for (int32_t j = 0; j < _dim; j++) {
+        mat_a[i * M + j] = bf16(queries[i][j]);
+      }
+    }
+
+    for (int32_t i = 0; i < _dataset.size(); i++) {
+      for (int32_t j = 0; j < _dim; j++) {
+        mat_b[i * M + j] = bf16(_dataset[i][j]);
+      }
+    }
+
+    amx_inner_product(
+        queries.size(), _dataset.size(), _dim, mat_a.data(), mat_b.data(), engine, stream, debug);
+
+
+    for (int32_t i = 0; i < distances.size(); i++) {
+        for (int32_t j = 0; j < distances[0].size(); j++) {
+            map[i].push(distances[i][j]);
+        }
+    }
+
+    for (int i = 0; i < queries.size(); i++) {
+      int32_t k_idx = 0;
+      while (k_idx < top_k) {
+        results[i][k_idx++] = map[i].top();
+        map[i].pop();
+      }
+    }
+    return results;
+  }
+};
---
+++ amx/distance.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <chrono>
+#include <immintrin.h>
+#include <unordered_map>
+#include <stdfloat>
+
+#include "oneapi/dnnl/dnnl.hpp"
+
+using tag = dnnl::memory::format_tag;
+using dt = dnnl::memory::data_type;
+using bf16 = std::bfloat16_t;
+
+static bool is_amxbf16_supported() {
+  unsigned int eax, ebx, ecx, edx;
+  __asm__ __volatile__("cpuid"
+                       : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+                       : "a"(7), "c"(0));
+  return edx & (1 << 22);
+}
+
+static void read_from_dnnl_memory(void *handle, dnnl::memory &mem) {
+  dnnl::engine eng = mem.get_engine();
+  int32_t size = mem.get_desc().get_size();
+  if (!handle)
+    throw std::runtime_error("handle is nullptr.");
+  uint8_t *src = static_cast<uint8_t *>(mem.get_data_handle());
+  if (!src)
+    throw std::runtime_error("get_data_handle returned nullptr.");
+  for (int32_t i = 0; i < size; ++i) {
+    ((uint8_t *)handle)[i] = src[i];
+  }
+}
+
+static void write_to_dnnl_memory(void const *handle, dnnl::memory &mem) {
+  dnnl::engine eng = mem.get_engine();
+  int32_t size = mem.get_desc().get_size();
+  if (!handle)
+    throw std::runtime_error("handle is nullptr.");
+  uint8_t *dst = static_cast<uint8_t *>(mem.get_data_handle());
+  if (!dst)
+    throw std::runtime_error("get_data_handle returned nullptr.");
+  for (int32_t i = 0; i < size; ++i) {
+    dst[i] = ((uint8_t *)handle)[i];
+  }
+}
+
+static void amx_inner_product(int32_t const &n, int32_t const &oc,
+                              int32_t const &ic, const bf16 *s, const bf16 *w,
+                              dnnl::engine &engine, dnnl::stream &stream) {
+  dnnl::memory::dims s_dims = {n, ic};
+  dnnl::memory::dims w_dims = {oc, ic};
+  dnnl::memory::dims dst_dims = {n, oc};
+
+  auto s_md = dnnl::memory::desc(s_dims, dt::bf16, tag::ab);
+  auto w_md = dnnl::memory::desc(w_dims, dt::bf16, tag::ab);
+  auto dst_md = dnnl::memory::desc(dst_dims, dt::bf16, tag::ab);
+  
+  auto s_mem = dnnl::memory(s_md, engine);
+  auto w_mem = dnnl::memory(w_md, engine);
+  write_to_dnnl_memory(s, s_mem);
+  write_to_dnnl_memory(w, w_mem);
+
+  auto pd = dnnl::inner_product_forward::primitive_desc(
+      engine, dnnl::prop_kind::forward_training, s_md, w_md, dst_md);
+  auto dst_mem = dnnl::memory(pd.dst_desc(), engine);
+
+  auto prim = dnnl::inner_product_forward(pd);
+  std::unordered_map<int32_t, dnnl::memory> args;
+  args.insert({DNNL_ARG_SRC, s_mem});
+  args.insert({DNNL_ARG_WEIGHTS, w_mem});
+  args.insert({DNNL_ARG_DST, dst_mem});
+  prim.execute(stream, args);
+  stream.wait();
+}
