ff42bee2f54424def174da8bfe6a8e16acd88bf5
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-02-19 13:55:30 +0530

    Fixes

--- amx/bf.hpp
+++ amx/bf.hpp
@@ -35,7 +35,8 @@ public:
   void search_ip_amx(
     std::vector<float> &queries, int32_t nq,
     std::vector<float> &dataset, int32_t nl, int32_t top_k) {
-    std::vector<bf16> results(queries.size() * top_k);
+
+    std::vector<bf16> distances(queries.size() * top_k);
     std::unordered_map<
       int32_t, 
       std::priority_queue<
@@ -57,24 +58,26 @@ public:
     }
 
     amx_inner_product(
-      nq, nl, _dim, mat_a, mat_b, results, engine, stream
+      nq, nl, _dim, mat_a, mat_b, distances, engine, stream
     );
-
-    std::cout << "calculated" << std::endl;
 
     for (int32_t i = 0; i < nq; i++) {
       for (int32_t j = 0; j < _dim; j++) {
-        m[i].push({j, results[i * _dim + j]});
+        m[i].push({j, distances[i * _dim + j]});
       }
     }
 
-    // for (int i = 0; i < queries.size(); i++) {
-    //   int32_t k_idx = 0;
-    //   while (k_idx < top_k) {
-    //     results[i][k_idx++] = map[i].top();
-    //     map[i].pop();
-    //   }
-    // }
+    std::vector<std::vector<int>> results(
+      nq, std::vector<int>(top_k)
+    );
+
+    for (int i = 0; i < queries.size(); i++) {
+      int32_t k_idx = 0;
+      while (k_idx < top_k) {
+        results[i][k_idx++] = map[i].top().first;
+        map[i].pop();
+      }
+    }
     // return results;
   }
 };
