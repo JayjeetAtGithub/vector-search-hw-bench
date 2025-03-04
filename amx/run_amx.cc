b56f805dc867ca9ae867ffefa732c2914c7d8197
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-04 13:57:10 -0800

    Debug

--- amx/run_amx.cc
+++ amx/run_amx.cc
@@ -5,6 +5,7 @@ int main() {
     int64_t learn_limit = 1000000;
     int64_t search_limit = 10000;
     int32_t dim = 200;
+    int32_t top_k = 10;
 
     std::string dataset_path_learn = "/workspace/dataset/deep1b/dataset.bin";
     int64_t n_learn, dim_learn;
@@ -15,10 +16,10 @@ int main() {
     auto data_query = read_bin_dataset(dataset_path_query.c_str(), &n_query, &dim_query, search_limit);
 
     auto bf_search = std::make_shared<BruteForceSearch>(dim);
-    auto results = bf_search->search_ip_amx(data_query, n_query, data_learn, n_learn, 10);
+    auto results = bf_search->search_ip_amx(data_query, n_query, data_learn, n_learn, top_k);
     std::cout << "Results preview: " << std::endl;
     for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < top_k; j++) {
             std::cout << results[i][j] << " ";
         }
         std::cout << std::endl;
