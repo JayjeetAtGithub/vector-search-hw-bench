860578602ae05db09432427ea6ce57bd2c7aac51
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-06 08:40:58 -0800

    Fix code

--- amx/distance.hpp
+++ amx/distance.hpp
@@ -28,38 +28,31 @@ static void amx_inner_product(int32_t const &n, int32_t const &oc,
 
   auto s_in_md = dnnl::memory::desc(s_dims, dt::f32, tag::ab);
   auto w_in_md = dnnl::memory::desc(w_dims, dt::f32, tag::ab);
-  auto res_out_md = dnnl::memory::desc(dst_dims, dt::f32, tag::ab);
-
+  auto dst_out_md = dnnl::memory::desc(dst_dims, dt::f32, tag::ab);
   auto s_in_mem = dnnl::memory(s_in_md, engine);
   auto w_in_mem = dnnl::memory(w_in_md, engine);
-
   write_to_dnnl_memory(s.data(), s_in_mem);
   write_to_dnnl_memory(w.data(), w_in_mem);
 
   auto s_md = dnnl::memory::desc(s_dims, dt::bf16, tag::any);
   auto w_md = dnnl::memory::desc(w_dims, dt::bf16, tag::any);
-  
-  auto s_mem = dnnl::memory(s_md, engine);
-  dnnl::reorder(s_in_mem, s_mem).execute(stream, s_in_mem, s_mem);
-  auto w_mem = dnnl::memory(w_md, engine);
-  dnnl::reorder(w_in_mem, w_mem).execute(stream, w_in_mem, w_mem);
-  auto res_mem = dnnl::memory(res_out_md, engine);
-  
-  auto pd = dnnl::inner_product_forward::primitive_desc(
-      engine, dnnl::prop_kind::forward_training, s_md, w_md, res_out_md);
 
+  auto pd = dnnl::inner_product_forward::primitive_desc(
+      engine, dnnl::prop_kind::forward_training, s_md, w_md, dst_out_md);
+  
+  auto s_mem = dnnl::memory(pd.src_desc(), engine);
+  dnnl::reorder(s_in_mem, s_mem).execute(stream, s_in_mem, s_mem);
+  auto w_mem = dnnl::memory(pd.weights_desc(), engine);
+  dnnl::reorder(w_in_mem, w_mem).execute(stream, w_in_mem, w_mem);
+  auto dst_mem = dnnl::memory(pd.dst_desc(), engine);
+  
   auto prim = dnnl::inner_product_forward(pd);
   std::unordered_map<int32_t, dnnl::memory> args;
   args.insert({DNNL_ARG_SRC, s_mem});
   args.insert({DNNL_ARG_WEIGHTS, w_mem});
-  args.insert({DNNL_ARG_DST, res_mem});
+  args.insert({DNNL_ARG_DST, dst_mem});
   prim.execute(stream, args);
   stream.wait();
 
-  auto st = std::chrono::high_resolution_clock::now();
   read_from_dnnl_memory(res.data(), res_mem);
-  auto en = std::chrono::high_resolution_clock::now();
-  std::cout << "[TIME] AMX: "
-            << std::chrono::duration_cast<std::chrono::milliseconds>(en - st).count()
-            << " ms" << std::endl;
 }
