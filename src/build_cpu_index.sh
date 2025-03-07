4943ec3f7126e56690100deb2021a26d247ef303
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-07 13:37:49 -0800

    Add search for 50M

--- src/build_cpu_index.sh
+++ src/build_cpu_index.sh
@@ -8,10 +8,8 @@ build_flat() {
     --index-type flat \
     --dataset-dir /workspace/dataset/t2i \
     --learn-limit ${1} \
-    --search-limit ${2} \
-    --top-k 10 \
     --metric ip \
-    --index-file cpu_flat_${1}l_${2}q.faiss
+    --index-file cpu_flat_${1}l.faiss
 }
 
 build_ivf() {
@@ -19,9 +17,8 @@ build_ivf() {
       --index-type ivf \
       --dataset-dir /workspace/dataset/t2i \
       --learn-limit ${1} \
-      --search-limit ${2} \
       --metric ip \
-      --index-file cpu_ivf_${1}l_${2}q.faiss
+      --index-file cpu_ivf_${1}l.faiss
 }
 
 build_hnsw() {
@@ -29,19 +26,10 @@ build_hnsw() {
       --index-type hnsw \
       --dataset-dir /workspace/dataset/t2i \
       --learn-limit ${1} \
-      --search-limit ${2} \
       --metric ip \
-      --index-file cpu_hnsw_${1}l_${2}q.faiss
+      --index-file cpu_hnsw_${1}l.faiss
 }
 
-build_flat 100000   10000 
-build_flat 1000000  10000
-build_flat 10000000 10000
-
-build_ivf  100000   10000
-build_ivf  1000000  10000
-build_ivf  10000000 10000
-
-build_hnsw 100000   10000
-build_hnsw 1000000  10000
-build_hnsw 10000000 10000
+build_flat  20000000
+build_ivf   20000000
+build_hnsw  20000000
