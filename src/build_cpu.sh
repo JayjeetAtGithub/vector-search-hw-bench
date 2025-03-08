465556ffacda26ec90136ff48f5ba30a26cff67c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-08 10:22:54 -0800

    Fixes

--- src/build_cpu.sh
+++ src/build_cpu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-# we build faiss specifically for the Sapphire Rapids CPU with the avx512 and spr flags
-g++ -std=c++17 -O3 run_cpu.cc -lfaiss_avx512_spr -march=sapphirerapids -o run_cpu
+g++ -std=c++17 -O3 run_cpu.cc -lfaiss_avx512 -o run_cpu
