761654441d88ea5ab30e5add1d994262104e3ba3
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-08 10:22:00 -0800

    Fixes

--- install_faiss_cpu_spr.sh
+++ install_faiss_cpu_spr.sh
@@ -14,10 +14,11 @@ cd build/
 sudo apt-get update
 sudo apt-get install -y libssl-dev libblas-dev libopenblas-dev liblapack-dev swig python3-numpy
 
+## turn off -DFAISS_OPT_LEVEL=avx2 on grace hopper systems
 cmake -DFAISS_ENABLE_GPU=OFF \
       -DFAISS_ENABLE_PYTHON=OFF \
       -DBUILD_SHARED_LIBS=ON \
-      -DFAISS_OPT_LEVEL=avx512 \
+      -DFAISS_OPT_LEVEL=avx512_spr \
       -DBUILD_TESTING=OFF \
       -DCMAKE_BUILD_TYPE=Release \
      ..
