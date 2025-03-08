b03ec29f3441414a8147d73c9eb228262ffa716a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-07 18:25:03 -0800

    Build flat and ivf indexes

--- src/build_gpu_index.sh
+++ src/build_gpu_index.sh
@@ -21,7 +21,11 @@ build_ivf() {
         --index-file gpu_ivf_${1}l.faiss
 }
 
-build_flat 20000000
-build_ivf  20000000
-build_flat 40000000
-build_ivf  40000000
+build_flat 100000
+build_ivf  100000
+
+build_flat 1000000
+build_ivf  1000000
+
+build_flat 10000000
+build_ivf  10000000
