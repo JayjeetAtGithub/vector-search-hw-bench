85105f141d2c4e78729b95f3156daaaa4c2c016b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-08 18:11:39 -0800

    Add code to generate groundtruth

---
+++ src/run_gen_gt.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+run_gen_gt() {
+    ./gen_gt \
+        --dataset-dir /workspace/dataset/t2i \
+        --learn-limit ${1} \
+        --search-limit ${2} \
+        --top-k 10 \
+        --metric ip
+}
+
+run_gen_gt 100000 1000
+run_gen_gt 100000 10000
+run_gen_gt 100000 100000
+
+run_gen_gt 1000000 1000
+run_gen_gt 1000000 10000
+run_gen_gt 1000000 100000
+
+run_gen_gt 10000000 1000
+run_gen_gt 10000000 10000
+run_gen_gt 10000000 100000
