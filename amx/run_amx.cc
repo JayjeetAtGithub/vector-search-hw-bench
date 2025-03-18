6775d5d708b0bfb9002975a84ea191866ddb9895
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-18 12:42:21 -0700

    debug

--- amx/run_amx.cc
+++ amx/run_amx.cc
@@ -1,11 +1,40 @@
 #include "bf.hpp"
 #include "utils.h"
+#include "CLI11.hpp"
 
 int main() {
-    int64_t learn_limit = 100000;
-    int64_t search_limit = 1000;
-    int32_t dim = 200;
-    int32_t top_k = 10;
+    CLI::App app{"Run FAISS Benchmarks"};
+    argv = app.ensure_utf8(argv);
+  
+    std::string index_type = "ivf";
+    app.add_option("--index-type", index_type, "Type of index to use (ivf, flat)");
+
+    std::string calc_recall = "false";
+    app.add_option("--calc-recall", calc_recall, "Calculate recall (true / false)");
+  
+    std::string dataset_dir;
+    app.add_option("-d,--dataset-dir", dataset_dir, "Path to the dataset");
+
+    int64_t learn_limit = 10000;
+    app.add_option("--learn-limit", learn_limit,
+                   "Limit the number of learn vectors");
+  
+    int64_t search_limit = 10000;
+    app.add_option("--search-limit", search_limit,
+                   "Limit the number of search vectors");
+  
+    int64_t top_k = 10;
+    app.add_option("-k,--top-k", top_k, "Number of nearest neighbors");
+  
+    int64_t n_probe = 32;
+    app.add_option("--n-probe", n_probe, "Number of probes");
+
+    CLI11_PARSE(app, argc, argv);
+  
+    if (dataset_dir.empty()) {
+      std::cerr << "[ERROR] Please provide a dataset" << std::endl;
+      return 1;
+    }
 
     std::string dataset_path_learn = "/workspace/dataset/t2i/dataset.bin";
     int64_t n_learn, dim_learn;
