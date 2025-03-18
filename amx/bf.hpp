62c4cfa454451ef97022fec568ad5b40aa40f33a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-18 09:48:00 -0700

    Fix path

--- amx/bf.hpp
+++ amx/bf.hpp
@@ -83,7 +83,7 @@ public:
     int qnn = 0;
     int idd = 0;
     while (!m[qnn].empty()) {
-      if (idd == 0) std::cout << "top of q: " m[qnn].top().first << " ";
+      if (idd == 0) std::cout << "top of q: " << m[qnn].top().first << " ";
       if (idd == 99999) std::cout << "last of q: " << m[qnn].top().first << std::endl;
       m[qnn].pop();
       idd++;
