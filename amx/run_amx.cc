81674710c50da711993ef3a8f5edf778fe838b84
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-18 09:16:46 -0700

    Fix path

--- amx/run_amx.cc
+++ amx/run_amx.cc
@@ -7,11 +7,11 @@ int main() {
     int32_t dim = 200;
     int32_t top_k = 10;
 
-    std::string dataset_path_learn = "/workspace/dataset/deep1b/dataset.bin";
+    std::string dataset_path_learn = "/workspace/dataset/t2i/dataset.bin";
     int64_t n_learn, dim_learn;
     auto data_learn = read_bin_dataset(dataset_path_learn.c_str(), &n_learn, &dim_learn, learn_limit);
     
-    std::string dataset_path_query =  "/workspace/dataset/deep1b/query.bin";
+    std::string dataset_path_query =  "/workspace/dataset/t2i/query.bin";
     int64_t n_query, dim_query;
     auto data_query = read_bin_dataset(dataset_path_query.c_str(), &n_query, &dim_query, search_limit);
 
