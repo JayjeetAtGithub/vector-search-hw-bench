ec1d9e79d58132c054299d72c0e3d2ae1df42dd6
Jayjeet Chakraborty <jc.github@rediffmail.com>
2025-03-21 09:09:34 -0700

    Add -fno-omit-frame-pointer

--- src/build_gpu.sh
+++ src/build_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-g++ -std=c++17 -O3 run_gpu.cc -lfaiss_avx512 -o run_gpu
-g++ -std=c++17 -O3 run_gen_gt.cc  -lfaiss_avx512 -o run_gen_gt
+g++ -std=c++17 -O3 -fno-omit-frame-pointer run_gpu.cc -lfaiss_avx512 -o run_gpu
+g++ -std=c++17 -O3 -fno-omit-frame-pointer run_gen_gt.cc  -lfaiss_avx512 -o run_gen_gt
